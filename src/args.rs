a0510aca681de8030a65b6aba63a560fecfed477
Benno Lang <transmogribenno@gmail.com>
2025-12-01 21:31:52 +1030

    Apply cargo fmt

--- src/args.rs
+++ src/args.rs
@@ -1,11 +1,13 @@
 use std::env;
 
 pub fn get_day_part() -> (u8, u8) {
-    let day: u8 = env::args().nth(1)
+    let day: u8 = env::args()
+        .nth(1)
         .unwrap_or("1".to_string())
         .parse()
         .expect("Failed to parse day arg");
-    let mut part: u8 = env::args().nth(2)
+    let mut part: u8 = env::args()
+        .nth(2)
         .unwrap_or("1".to_string())
         .parse()
         .expect("Failed to parse part arg");
